1c4f04bb39485b99f218a8d8b5db1a158abee22c
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-12-07 19:48:50 +1300

    show results in table

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,33 +4,38 @@ var app = angular.module('searchbrew', []);
 
 app.controller('SearchCtrl', function ($scope, $http, $timeout) {
         $scope.showAll = false;
+        $scope.loading = false;
 
         function doSearch(){
+            if (!$scope.searchQuery) return;
+            $scope.loading = true;
             $scope.showAll = false;
-            if (!$scope.searchString) return;
-            $http.get('/search?q='+$scope.searchString).
+            $http.get('/search?q='+$scope.searchQuery).
                 success(function(data){
-                    if(data.query === $scope.searchString){
-                        $scope.results = data;
+                    if(data.query === $scope.searchQuery){
+                        $scope.loading = false;
+                        $scope.searchResults = data.hits.hits;
                     }
                 });
         };
 
         var timeout;
-        $scope.$watch('searchString', function(){
+        $scope.$watch('searchQuery', function(){
             if (timeout) $timeout.cancel(timeout);
-            if ($scope.searchString && $scope.searchString.length > 0){
+            if ($scope.searchQuery && $scope.searchQuery.length > 0){
                 timeout = $timeout(doSearch, 150);
             } else {
-                $scope.results = {};
+                $scope.searchResults = [];
             }
         });
 
         $scope.getAll = function(){
             $scope.showAll = true;
+            $scope.searchQuery = "";
+            $scope.loading = true;
             $http.get('/search?size=10000').
                 success(function(data){
-                    $scope.results = data;
+                    $scope.loading = false;
                     $scope.searchResults = data.hits.hits;
                 });
         };
