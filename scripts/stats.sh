78d91673ea325867fe09c51bb9a40cc76b78c682
Stephen Nancekivell <stephennancekivell@gmail.com>
2014-02-06 20:42:26 +1100

    added stats strips

---
+++ scripts/stats.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ssh searchbrew.com /bin/sh << EOF
+	date
+	grep "GET /search?q=" /var/log/apache2/searchbrew.com.access.log |\
+		grep -v downnotifier |\
+		tail -n 25 |\
+		cut -d ' ' -f 1,4,7 |\
+		tr -s ' ' '\t'
+	echo "\nError"
+	tail -n 5 /var/log/apache2/searchbrew.com.error.log
+EOF
