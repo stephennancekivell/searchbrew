d3f63c23ab31b820f19046c2eaa5d599a4b88db4
Stephen Nancekivell <stephen@redballoon.com.au>
2014-03-04 21:47:41 +1100

    stats has logs

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -3,11 +3,15 @@ ssh searchbrew.com /bin/sh << EOF
 	date
 	grep "GET /search?q=" /var/log/apache2/searchbrew.com.access.log |\
 		grep -v downnotifier |\
-		tail -n 25 |\
+		tail -n 10 |\
 		cut -d ' ' -f 1,4,7 |\
 		tr -s ' ' '\t'
 	echo "\nError"
 	tail -n 5 /var/log/apache2/searchbrew.com.error.log
+	echo "\n Server"
+	tail -n 5 /home/searchbrew/logs/server.log | cut -b 1-100
+	echo "\n Updater"
+	tail -n 5 /home/searchbrew/logs/updater.log
 	echo "\nloadavg "
 	cat /proc/loadavg
 
