6f0788dd81491baf06a8cd6eecca9e7b21ef3ad4
Stephen Nancekivell <stephen@redballoon.com.au>
2014-07-19 18:43:17 +1000

    update stats.sh

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -8,14 +8,10 @@ ssh searchbrew.com /bin/sh << EOF
 		tr -s ' ' '\t'
 	echo "\nError"
 	tail -n 5 /var/log/apache2/searchbrew.com.error.log
-	echo "\n Server"
-	tail -n 5 /home/searchbrew/logs/server.log | cut -b 1-100
-	echo "\n Updater"
-	tail -n 5 /home/searchbrew/logs/updater.log
+	echo "\nServer"
+	tail -n 5 /home/searchbrew/logs/application.log | cut -b 1-100
 	echo "\nloadavg "
 	cat /proc/loadavg
 
 	service searchbrew status
-	service searchbrew.updater status
-	sudo service elasticsearch status
 EOF
