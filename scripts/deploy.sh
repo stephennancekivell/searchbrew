9f890523f11245cbb599553ed52fbad0f4af948e
Stephen Nancekivell <stephen@redballoon.com.au>
2014-07-19 18:37:06 +1000

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,27 +2,31 @@
 
 SCRIPT_DIR=`dirname $0`
 
-SERVER=searchbrew.com
+SERVER=new.searchbrew.com
 TARGET=/home/searchbrew
 SERVICE_SERVER=searchbrew
 INIT_SERVER_CONF=searchbrew.conf
-APACHE_CONF=searchbrew.com
+APACHE_CONF=searchbrew.com.conf
 USER=searchbrew
 
 cd $SCRIPT_DIR/../server
-sbt clean stage
+#sbt clean stage
 cd ..
 
 ssh $SERVER <<EOF
 	sudo service $SERVICE_SERVER stop
 
-	if [ ! -f $TARGET ]; then
-		sudo adduser $USER
-	fi
+	# if [ ! -f $TARGET ]; then
+	# 	sudo adduser $USER
+	# 	sudo mkdir $TARGET/.ssh
+	# 	sudo cp ~/.ssh/authorized_keys $TARGET/.ssh/
+	# 	sudo chown -R $USER:$USER $TARGET/.ssh
+	# 	sudo sh -c "echo 'searchbrew.com' > /etc/hostname"
+	# fi
 
 	# install required
 	sudo add-apt-repository ppa:webupd8team/java
-	sudo apt-get update
+	#sudo apt-get update
 	sudo apt-get -y install apache2 unattended-upgrades oracle-java7-installer oracle-java7-set-default git
 
 	sudo a2enmod proxy_http
@@ -34,7 +38,7 @@ ssh $USER@$SERVER <<EOF
 	mkdir -p $TARGET/server/dist
 EOF
 
-rsync -v --recursive --delete --compress $SCRIPT_DIR/../server/target/universal/stage/ $USER@$SERVER:/$TARGET/server/dist/
+rsync -v --recursive --exclude git.repo --delete --compress $SCRIPT_DIR/../server/target/universal/stage/ $USER@$SERVER:/$TARGET/server/dist/
 
 ssh $USER@$SERVER <<EOF
 	ln -s $TARGET/logs $TARGET/server/dist
@@ -46,6 +50,7 @@ scp $SCRIPT_DIR/$APACHE_CONF $SERVER:.
 ssh $SERVER <<EOF
 	sudo mv $INIT_SERVER_CONF /etc/init/
 	sudo mv $APACHE_CONF /etc/apache2/sites-available
+	sudo a2dissite 000-default
 	sudo a2ensite $APACHE_CONF
 	sudo service apache2 reload
 	sudo service $SERVICE_SERVER start
