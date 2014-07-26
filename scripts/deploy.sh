83fd2e0f995e224c980c72e5ec506e5a94cf56da
Stephen Nancekivell <stephen@redballoon.com.au>
2014-07-26 12:44:28 +1000

    increase jvm max memory. add newrelic for the server.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,6 +29,15 @@ ssh $SERVER <<EOF
 	#sudo apt-get update
 	sudo apt-get -y install apache2 unattended-upgrades oracle-java7-installer oracle-java7-set-default git
 
+	sudo sh <<EOF
+		echo deb http://apt.newrelic.com/debian/ newrelic non-free > /etc/apt/sources.list.d/newrelic.list
+		wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
+		apt-get update
+		apt-get install newrelic-sysmond
+		#nrsysmond-config --set license_key=
+		/etc/init.d/newrelic-sysmond start
+	EOF
+
 	sudo a2enmod proxy_http
 	sudo a2enmod rewrite
 EOF
